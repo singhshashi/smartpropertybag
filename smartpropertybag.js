fbf2f746eca9cff29a26da73f60a2751969353a8
Shashi Singh <singh.shashishekhar@gmail.com>
2016-04-12 18:39:58 +0530

    Fixed alignment and bug due to typo

--- smartpropertybag.js
+++ smartpropertybag.js
@@ -1,58 +1,58 @@
-martpropertybag = function () {
-	  this.obj = {};
+smartpropertybag = function () {
+	this.obj = {};
 };
 
 
 smartpropertybag.prototype.defineSmartProperty = function(propertyName, defaultValue, persistent) {
-	  var valueType = typeof defaultValue;
-	    var descriptor = {configurable:false,enumerable:true};
-	      if (persistent === true) {
-		          if (typeof localStorage === 'undefined') {
-				        throw new Error('Cannot persist properties when localStorage is not available!');
-					    }
-			      switch (valueType) {
-				            case "boolean":
-						            descriptor.get = function() {
-								              return localStorage.getItem(propertyName) != null ? Boolean(localStorage.getItem(propertyName)):defaultValue;
-									              };
-							            descriptor.set = function(value) {
-									              localStorage.setItem(propertyName,value);
-										              }
-								            break;
-									          case "number":
-									            descriptor.get = function() {
-											              var result = parseInt(localStorage.getItem(propertyName));
-												                return !isNaN(result) ? result : defaultValue;
-														        };
-										            descriptor.set = function(value) {
-												              localStorage.setItem(propertyName,value);
-													              }
-											            break;
-												          case "string":
-												            descriptor.get = function() {
-														              return localStorage.getItem(propertyName) != null ? localStorage.getItem(propertyName) : defaultValue;
-															              };
-													            descriptor.set = function(value) {
-															              localStorage.setItem(propertyName,value);
-																              }
-														            break;
-															          case "object":
-															            descriptor.get = function() {
-																	              return localStorage.getItem(propertyName) != null ? JSON.parse(localStorage.getItem(propertyName)) : defaultValue;
-																		              };
-																            descriptor.set = function(value) {
-																		              localStorage.setItem(propertyName,JSON.stringify(value));
-																			              }
-																	            break;
+	var valueType = typeof defaultValue;
+	var descriptor = {configurable:false,enumerable:true};
+	if (persistent === true) {
+		if (typeof localStorage === 'undefined') {
+			throw new Error('Cannot persist properties when localStorage is not available!');
+		}
+		switch (valueType) {
+			case "boolean":
+				descriptor.get = function() {
+					return localStorage.getItem(propertyName) != null ? Boolean(localStorage.getItem(propertyName)):defaultValue;
+				};
+				descriptor.set = function(value) {
+					localStorage.setItem(propertyName,value);
+				}
+				break;
+			case "number":
+				descriptor.get = function() {
+					var result = parseInt(localStorage.getItem(propertyName));
+					return !isNaN(result) ? result : defaultValue;
+				};
+				descriptor.set = function(value) {
+					localStorage.setItem(propertyName,value);
+				}
+				break;
+			case "string":
+				descriptor.get = function() {
+					return localStorage.getItem(propertyName) != null ? localStorage.getItem(propertyName) : defaultValue;
+				};
+				descriptor.set = function(value) {
+					localStorage.setItem(propertyName,value);
+				}
+				break;
+			case "object":
+				descriptor.get = function() {
+					return localStorage.getItem(propertyName) != null ? JSON.parse(localStorage.getItem(propertyName)) : defaultValue;
+				};
+				descriptor.set = function(value) {
+					localStorage.setItem(propertyName,JSON.stringify(value));
+				}
+				break;
 
-																		          default:
-																		            break
-																				        }
-			        } else {
-					    descriptor.value = defaultValue;
-					        descriptor.writable = true;
-						  }
-	        Object.defineProperty(this, propertyName,descriptor);
+			default:
+				break
+		}
+	} else {
+		descriptor.value = defaultValue;
+		descriptor.writable = true;
+	}
+	Object.defineProperty(this, propertyName,descriptor);
 }
 
 
